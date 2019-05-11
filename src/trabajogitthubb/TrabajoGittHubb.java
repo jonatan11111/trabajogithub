fe7586b71a84835996d430969fc50edb36e0f578
Jonatan <Jonatan@Benjamin>
2019-05-11 13:24:23 +0200

    5º versión trabajo entornos GitHub

---
+++ src/trabajogitthubb/TrabajoGittHubb.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajogitthubb;
+
+/**
+ *
+ * @author Jonatan y David
+ */
+public class TrabajoGittHubb{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1=30;
+        int n2=7;
+        int resul;
+        
+        resul=n1*n2;
+        
+        System.out.print("La multiplicación de " + n1 + " x " + n2 + " = " + resul);
+                
+                }
+}
